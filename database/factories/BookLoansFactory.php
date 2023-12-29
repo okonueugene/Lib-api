680329a3fff38b845216dabec48a048cb5201613
okonueugene <tylarrex67@gmail.com>
2023-12-29 09:39:52 +0300

    loan books seeder

--- database/factories/BookLoansFactory.php
+++ database/factories/BookLoansFactory.php
@@ -19,36 +19,15 @@ class BookLoansFactory extends Factory
 
 
     protected $model = BookLoans::class;
-    //     $table->foreignId('book_id')
-    //     ->constrained()
-    //     ->onDelete('cascade');
-    // $table->foreignId('user_id')
-    //     ->constrained()
-    //     ->onDelete('cascade');
-    // $table->date('can_date')->nullable();
-    // $table->date('due_date')->nullable();
-    // $table->date('return_date')
-    //     ->nullable();
-    // $table->string('extended', 3)
-    //     ->default('no');
-    // $table->string('extension_tale_cate', 200)
-    //     ->nullable();
-    // $table->string('penalty_amount')
-    //     ->nullable();
-    // $table->string('penalty_status', 15)->default('unpaid');
-    // $table->string('status')->default('pending');
-    // $table->unsignedBigInteger('added_by')->nullable();
-    // $table->unsignedBigInteger('updated_by')->nullable();
-    // $table->timestamps();
+
     public function definition(): array
     {
         return [
             'book_id' => fake()->randomElement(Book::pluck('id')->toArray()),
             'user_id' => fake()->randomElement(User::pluck('id')->toArray()),
             'can_date' => now(),
-            //future date
             'due_date' => now()->addDays(7),
-            'return_date' => now()->addDays(7),
+            'return_date' => randomElement([now()->addDays(13), null]),
             'extended' => randomElement(['yes', 'no']),
             'extension_tale_cate' => $this->faker->sentence,
             'penalty_amount' => fake()->randomElement([null,'100', '200', '300', '400', '500']),
